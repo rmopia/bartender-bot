6133a962ac3a168a69e787c77f899ca5b3692014
Robert Mopia <53323390+rmopia@users.noreply.github.com>
2019-07-30 11:16:44 -0700

    Added more dialogue, fixed bugs

--- bar_bot.py
+++ bar_bot.py
@@ -2,33 +2,45 @@
 
 from nltk.chat.util import Chat, reflections
 
-user_greetings = (r"Hi", r"Hello", r"Howdy")
 bar_bot_greetings = ["How do you do?", "Mhmm.", "Parched?", "Fancy a drink?",
-                     "You new around these parts?"]
-bar_bot_farewells = ["Fair enough.", "Salutations, friend.", "That's just how it is." 
-                     "Bye now, cowpoke.", "A damn shame."]
+                     "You new around these parts?", "Howdy, there."]
+bar_bot_farewells = ["Fair enough.", "Salutations, friend.", "That's just how it is.",
+                     "Bye now, cowpoke.", "A damn shame.", "Safe travels."]
 bar_bot_neutral = ["Good for you.", "Alrighty, then.", "*Nods*", "Interesting.",
                    "Uh-huh."]
 
 dialogue = (
     (
-        r"hi", (bar_bot_greetings)
+        r"hi(.*)", (bar_bot_greetings)
+    ),
+    (
+        r"howdy(.*)", (bar_bot_greetings)
     ),
     (
         r"(.*) whiskey(.*)", ("%1 whiskey coming right up.", "Sure thing cowpoke.")
     ),
     (
-        r"(.*) have?", ("We got beer, spirits and a whole lotta whiskey.")
+        r"(.*)beer(.*)", ("Coming right up.", "Consider it done.", "A beer coming your way.")
     ),
     (
-        r"(.*)recommend(.*)", ("Other than whiskey, An old Chattanooga beer.")
+        r"(.*)spirits?(.*)", ("We carry gin, tequila and most of all: whiskey.",)
     ),
     (
-        r"where am(.*)", ("The Salty Spitoon.", "The one and only: Salty Spitoon.")
+        r"(.*) have?", ("We got beer, spirits and a whole lotta whiskey.",)
+    ),
+    (
+        r"(.*)recommend(.*)", ("Other than whiskey, An old Chattanooga beer.",
+                               "Other than the whiskey, A fried skillet.")
+    ),
+    (
+        r"where am(.*)", ("The Salty Spitoon.", "The one and the only: Salty Spitoon.")
     ),
     (
         r"(.*)\?", ("What was that?", "Can you repeat the question?",
-                   "I didn't quite catch that.", "Could you speak a little louder?")
+                    "I didn't quite catch that.", "Could you speak a little louder?")
+    ),
+    (
+        r"bye", (bar_bot_farewells)
     ),
     (
         r"quit", (bar_bot_farewells)
@@ -45,7 +57,7 @@ def bar_demo():
     print("Welcome to the Salty Spitoon. Home of the best whiskey for the next 100 miles!")
     print("*" * 78)
     print("'Howdy cowpoke. What can I get you?'")
-
     bar_bot.converse()
+
 
 bar_demo()
