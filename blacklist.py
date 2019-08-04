c6340baa8cae8979cacfdde385c59b2f73b6b1b9
Robert Mopia <53323390+rmopia@users.noreply.github.com>
2019-08-03 20:45:47 -0700

    Create blacklist.py

---
+++ blacklist.py
@@ -0,0 +1,8 @@
+"""profanity leads to the program kicking you out"""
+BLACKLIST = {"boob",
+             "gook",
+             "bitch",
+             "whore",
+             "tit",
+             "slut",
+             "cunt"}
